2b9f8c21c674953ad166517dd45ce6df0ff64ded
최재우 <choizeus@choizeus-MacMini.local>
2026-04-28 00:48:26 +0900

    chore: trigger ci build

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import FastAPI  # noqa
 from fastapi.middleware.cors import CORSMiddleware
 
 from database import init_db
